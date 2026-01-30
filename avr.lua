22bcc9d4eabc100789baa21e3b246be23316aab4
lucasFelixSilveira <lucasdwbfff@gmail.com>
2026-01-30 15:59:33 -0300

    Basics MCUs intructions!

--- avr.lua
+++ avr.lua
@@ -3,6 +3,19 @@ local core = morgana.require 'core'
 local symbols = morgana.require 'symbols'
 
 local sample = false
+local loop = 0;
+
+function ms_to_registers(ms)
+    local value = ms % 65536
+    local low = value % 256
+    local high = math.floor(value / 256)
+
+    local hex_low = string.format("0x%02X", low)
+    local hex_high = string.format("0x%02X", high)
+
+    return hex_low, hex_high, low, high
+end
+
 
 function parse(entries)
     local code = ""
@@ -18,7 +31,7 @@ function parse(entries)
         local data = morgana.next()
         if not data then break end
 
-        if data.kind == 5 and entries then
+        if data.kind == 1002 and entries then
             append('sbi 0x0A, ' .. data.pin .. '\n')
 
             if data.toggle then append('sbi 0x0B, ' .. data.pin .. '\n')
@@ -31,9 +44,23 @@ function parse(entries)
             append '.section .text\n'
             append('.global ' .. data.name .. '\n')
             append(data.name .. ':\n')
-
             append(parse(true))
+            goto continue
+        end
 
+        if data.kind == 4 and entries then
+            code = code .. '.LOOP' .. loop .. ':\n'
+            code = code .. parse(true)
+            append('rjmp .LOOP' .. loop .. '\n')
+            loop = loop + 1;
+            goto continue
+        end
+
+        if data.kind == 5 or data.kind == 6 then
+            local r24, r25 = ms_to_registers(data.ms)
+            append('ldi r24, ' .. r24 .. '\n')
+            append('ldi r25, ' .. r25 .. '\n')
+            append('rcall morgana_delay_ms\n')
             goto continue
         end
 
@@ -49,6 +76,26 @@ function codegen()
     local append = function(x)
         code = code .. x
     end
+
+    append [[.global morgana_delay_ms
+morgana_delay_ms:
+    movw r30, r24
+    or r30, r31
+    breq morgana_delay_end
+
+morgana_delay_loop:
+    ldi r18, 200
+1:  ldi r19, 10
+2:  nop
+    dec r19
+    brne 2b
+    dec r18
+    brne 1b
+    sbiw r24, 1
+    brne morgana_delay_loop
+morgana_delay_end:
+    ret
+]]
 
     symbols:newScope()
 
