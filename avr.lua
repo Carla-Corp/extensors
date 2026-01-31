11dc79ab15bbd95d1156a89780b455fdaa273fe7
lucasFelixSilveira <lucasdwbfff@gmail.com>
2026-01-31 17:02:11 -0300

    br, brnez, labels was added. Improved the wait and waitms statement

--- avr.lua
+++ avr.lua
@@ -2,8 +2,14 @@ local json = morgana.require 'json'
 local core = morgana.require 'core'
 local symbols = morgana.require 'symbols'
 
+local current_function = ''
 local sample = false
 local loop = 0;
+
+function getPinMask(pin)
+    if pin < 0 or pin > 7 then error("Pin must be between 0 and 7") end
+    return 1 << pin
+end
 
 function ms_to_registers(ms)
     local value = ms % 65536
@@ -16,6 +22,7 @@ function ms_to_registers(ms)
     return hex_low, hex_high, low, high
 end
 
+local already_defined_gpios = {};
 
 function parse(entries)
     local code = ""
@@ -32,7 +39,10 @@ function parse(entries)
         if not data then break end
 
         if data.kind == 1002 and entries then
-            append('sbi 0x0A, ' .. data.pin .. '\n')
+            if not already_defined_gpios[data.pin] then
+                already_defined_gpios[data.pin] = true
+                append('sbi 0x0A, ' .. data.pin .. '\n')
+            end
 
             if data.toggle then append('sbi 0x0B, ' .. data.pin .. '\n')
             else                append('cbi 0x0B, ' .. data.pin .. '\n') end
@@ -40,15 +50,31 @@ function parse(entries)
             goto continue
         end
 
+        if data.kind == 1003 and entries then
+            if not already_defined_gpios[data.pin] then
+                already_defined_gpios[data.pin] = true
+                append('cbi 0x0A, ' .. data.pin .. '\n')
+                append('sbi 0x0B, ' .. data.pin .. '\n')
+            end
+
+            append 'in r24, 0x09\n'
+            append('andi r24, ' .. getPinMask(data.pin) .. '\n')
+            -- for i = 0, (data.pin - 1) do append 'lsr r24\n' end
+
+            -- append 'st X+, r24\n'
+            goto continue
+        end
+
         if data.kind == 1 and not entries then
             append '.section .text\n'
             append('.global ' .. data.name .. '\n')
             append(data.name .. ':\n')
+            current_function = data.name;
             append(parse(true))
             goto continue
         end
 
-        if data.kind == 4 and entries then
+        if data.kind == 5 and entries then
             code = code .. '.LOOP' .. loop .. ':\n'
             code = code .. parse(true)
             append('rjmp .LOOP' .. loop .. '\n')
@@ -56,11 +82,30 @@ function parse(entries)
             goto continue
         end
 
-        if data.kind == 5 or data.kind == 6 then
+        if data.kind == 6 or data.kind == 7 then
             local r24, r25 = ms_to_registers(data.ms)
             append('ldi r24, ' .. r24 .. '\n')
             append('ldi r25, ' .. r25 .. '\n')
             append('rcall morgana_delay_ms\n')
+            goto continue
+        end
+
+        if data.kind == 10 and entries then
+            code = code .. '.' .. current_function .. '_' .. data.identifier .. ':\n'
+            goto continue
+        end
+
+        if data.kind == 8 and entries then
+            -- needs make a good stack yet
+            -- append('ld r24, X+\n')
+            append('cpi r24, 0\n')
+            append('brne .' .. current_function .. '_' .. data.label .. '\n')
+            goto continue
+        end
+
+        if data.kind == 9 and entries then
+            -- needs make a good stack yet
+            append('rjmp .' .. current_function .. '_' .. data.label .. '\n')
             goto continue
         end
 
@@ -96,6 +141,7 @@ morgana_delay_loop:
 morgana_delay_end:
     ret
 ]]
+    append '\n'
 
     symbols:newScope()
 
